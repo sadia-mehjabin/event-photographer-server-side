a3cef0c44699859f171421cc0d9eaec4afae836a
sadia-mehjabin <jhidnicraft@gmail.com>
2022-11-09 02:05:20 +0600

    review data api set

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -13,7 +13,55 @@ require('dotenv').config();
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.uzz7izn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const serviceCollections = client.db('eventPhoto').collection('photoServices');
+        const reviewCollections = client.db('eventPhoto').collection('reviews');
 
+        app.get('/', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollections.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollections.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+        app.post('/services', async(req, res) => {
+            const newService = req.body;
+            const result = await serviceCollections.insertOne(newService);
+            res.send(result)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const service = await serviceCollections.findOne(query);
+            res.send(service)
+        })
+
+        app.get('/review', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollections.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews)
+        })
+
+        app.post('/review', async(req, res) => {
+            const newReview = req.body;
+            const result = await reviewCollections.insertOne(newReview);
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.log(error))
 
 app.get('/', (req, res) => {
     res.send('hello')
