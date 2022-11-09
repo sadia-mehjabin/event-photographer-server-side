2c04b25c65560dae2c09aa54c4c5b3d50178c0f0
sadia-mehjabin <jhidnicraft@gmail.com>
2022-11-09 23:57:22 +0600

    got token

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ async function run(){
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+            res.send({token})
         })
 
         app.get('/', async (req, res) => {
